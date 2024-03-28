ca8d52b504768b00ef1833c0841057b47f4df999
Yasir Barlas <79994089+yasirbarlas@users.noreply.github.com>
2024-03-28 04:07:04 +0000

    Add files via upload

--- SRGAN/dataset.py
+++ SRGAN/dataset.py
@@ -11,19 +11,22 @@ import numpy as np
 from torchvision import transforms
 import config
 
+# Allows for multiple datasets in different folders, e.g. ["DIV2K_train_HR", "Flickr2K"]
 class SuperResolutionDataset(Dataset):
-    def __init__(self, root_dir):
+    def __init__(self, dataset_folders, root_dir = os.getcwd()):
         super(SuperResolutionDataset, self).__init__()
         self.data = []
-        self.root_dir = root_dir  #os.path.join(root_dir, 'image_pairs')  # Assuming image_pairs subfolder
+        self.root_dir = root_dir
 
+        #os.path.join(root_dir, 'image_pairs')  # Assuming image_pairs subfolder
         # hr_img_dir = self.root_dir
-        hr_img_dir = os.path.join(self.root_dir,"hr_img")
+        # hr_img_dir = os.path.join(self.root_dir, "hr_img")
 
-
-        for hr_img in sorted(os.listdir(hr_img_dir)):
-          hr_img_path = os.path.join(hr_img_dir, hr_img)
-          self.data.append(hr_img_path)
+        for dataset_folder in dataset_folders:
+            dataset_dir = os.path.join(root_dir, dataset_folder)
+            for hr_img in sorted(os.listdir(dataset_dir)):
+                hr_img_path = os.path.join(dataset_dir, hr_img)
+                self.data.append(hr_img_path)
 
     def __len__(self):
         return len(self.data)
@@ -32,10 +35,9 @@ class SuperResolutionDataset(Dataset):
         img_path = self.data[index]
         img = np.array(Image.open(img_path))
 
-        transformed_img = config.both_transforms(image = img)['image']
-        lowres_image = config.lowres_transform(image = transformed_img)['image']
-        highres_image = config.highres_transform(image = transformed_img)['image']
+        transformed_img = config.both_transforms(image=img)['image']
+        lowres_image = config.lowres_transform(image=transformed_img)['image']
+        highres_image = config.highres_transform(image=transformed_img)['image']
         return lowres_image, highres_image
-    
 
 # data = SuperResolutionDataset(root_dir="G:\\My Drive\\GAN\\")
