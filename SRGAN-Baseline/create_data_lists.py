2bb88893c6d17a6ccd5bc0d6d5cab7b370fef649
Yasir Barlas <79994089+yasirbarlas@users.noreply.github.com>
2024-04-08 16:32:42 +0100

    Add files via upload

---
+++ SRGAN-Baseline/create_data_lists.py
@@ -0,0 +1,4 @@
+from utils import create_data_lists
+
+if __name__ == "__main__":
+    create_data_lists(train_folders = ["../DIV2K_train_HR"], val_folders = ["../Set14"], test_folders = ["../BSDS100"], min_size = 100, output_folder = "./")
