103905b8304b7800ca65245f6898abf4e30d6534
Rafi Patel <rafipatel3562@gmail.com>
2024-05-03 16:01:41 +0100

    changes

--- SRGAN-Baseline/create_data_lists.py
+++ SRGAN-Baseline/create_data_lists.py
@@ -1,4 +1,5 @@
 from utils import create_data_lists
 
 if __name__ == "__main__":
+    # create_data_lists(train_folders = ["../test2015"], val_folders = ["../val2017"], test_folders = ["../Set5", "../Set14", "../BSDS100"], min_size = 100, output_folder = "./")
     create_data_lists(train_folders = ["../test2015"], val_folders = ["../val2017"], test_folders = ["../Set5", "../Set14", "../BSDS100"], min_size = 100, output_folder = "./")
