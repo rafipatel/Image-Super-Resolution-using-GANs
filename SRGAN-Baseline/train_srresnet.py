be0c402258a09e055a3d9f0103b3b98b6ce838cf
Rafi Patel <rafipatel3562@gmail.com>
2024-05-05 06:08:15 +0100

    fixing

--- SRGAN-Baseline/train_srresnet.py
+++ SRGAN-Baseline/train_srresnet.py
@@ -337,10 +337,10 @@ def main():
     logger = wandb_logger.get_logger()
 
     # Custom dataloaders
-    train_dataset = SRDataset(data_folder = data_settings["data_folder"], split = "trains", crop_size = data_settings["crop_size"], scaling_factor = data_settings["scaling_factor"], lr_img_type = "imagenet-norm", hr_img_type = "[-1, 1]")
+    train_dataset = SRDataset(data_folder = data_settings["data_folder"], split = "train", crop_size = data_settings["crop_size"], scaling_factor = data_settings["scaling_factor"], lr_img_type = "imagenet-norm", hr_img_type = "[-1, 1]")
     train_dataloader = DataLoader(train_dataset, batch_size = srresnet_settings["batch_size"], shuffle = True, num_workers = srresnet_settings["workers"], pin_memory = True)  # note that we are passing the collate function here
 
-    val_dataset = SRDataset(data_folder = data_settings["data_folder"], split = "vals", crop_size = 0, scaling_factor = data_settings["scaling_factor"], lr_img_type = "imagenet-norm", hr_img_type = "[-1, 1]")
+    val_dataset = SRDataset(data_folder = data_settings["data_folder"], split = "val", crop_size = 0, scaling_factor = data_settings["scaling_factor"], lr_img_type = "imagenet-norm", hr_img_type = "[-1, 1]")
     val_dataloader = DataLoader(val_dataset, batch_size = 1, shuffle = True, num_workers = srresnet_settings["workers"], pin_memory = True)  # note that we are passing the collate function here
 
     checkpoint = None
